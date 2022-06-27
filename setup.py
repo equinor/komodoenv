6666e076ff89daae4423362bf45b5735409d59ea
Zohar Malamant <zom@equinor.com>
2022-06-27 11:06:58 +0200

    Add automatic downloading of bundled packages

--- setup.py
+++ setup.py
@@ -1,5 +1,28 @@
+import os
+import sys
 from setuptools import setup
+from subprocess import check_output
 
+
+def download_bundled_wheels() -> None:
+    dest = os.path.join(os.path.dirname(__file__), "komodoenv", "bundle")
+    print(f"Downloading wheels to {os.path.realpath(dest)}")
+    check_output(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "download",
+            "pip",
+            "setuptools",
+            "wheel",
+            "--dest",
+            dest,
+        ]
+    )
+
+
+download_bundled_wheels()
 
 setup(
     name="komodoenv",
