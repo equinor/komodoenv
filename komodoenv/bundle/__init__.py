b35f7824d8ebbe1c31c9b7d03af64a0fc32c0ee0
Zohar Malamant <zom@equinor.com>
2022-06-27 11:06:58 +0200

    Add automatic downloading of bundled packages

--- komodoenv/bundle/__init__.py
+++ komodoenv/bundle/__init__.py
@@ -1,13 +1,8 @@
 from pathlib import Path
 
 
-# Virtualenv provides several versions of these packages in order to support 2.7
-# and 3.4. We don't support these Pythons so we only provide a single version for
-# each package.
 BUNDLES = {
-    "pip": "pip-20.1.1-py2.py3-none-any.whl",
-    "setuptools": "setuptools-47.3.1-py3-none-any.whl",
-    "wheel": "wheel-0.34.2-py2.py3-none-any.whl",
+    wheel.name.split("-")[0]: wheel for wheel in Path(__file__).parent.glob("*.whl")
 }
 
 
@@ -15,4 +10,4 @@ def get_bundled_wheel(package: str) -> Path:
     """
     Returns path to a bundled wheel for 'package' (one of: pip, setuptools, wheel)
     """
-    return Path(__file__).parent / BUNDLES[package]
+    return BUNDLES[package]
